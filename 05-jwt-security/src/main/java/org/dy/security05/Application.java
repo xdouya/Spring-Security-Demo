b91da2607653f5e29f2187edf5f12c777cf9376e
caiwl <caiwenlao@sina.com>
2020-08-25 18:19:38 +0800

    新增jwt

---
+++ 05-jwt-security/src/main/java/org/dy/security05/Application.java
@@ -0,0 +1,13 @@
+package org.dy.security05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
